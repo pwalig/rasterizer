8f8a6bcc7cbf4cf04474ccb5498bb562fcdb07b1
Paweł <pwalig03@gmail.com>
2025-06-24 15:25:11 +0200

    Load scene from file

--- src/main.cpp
+++ src/main.cpp
@@ -37,19 +37,198 @@
 #include "thread_pool.hpp"
 #include "game/fly_cam.hpp"
 
-/* We will use this renderer to draw into this window every frame. */
 //using GBuffer = rast::image<rast::shader::deferred::first_pass::fragment::output>;
 //static GBuffer g_buffer;
-static rast::image<rast::color::rgba8> texture;
-static rast::image<rast::color::rgba8> texture2;
-static rast::mesh::indexed<rast::shader::inputs::position_normal_uv> icosphere;
-static rast::mesh::indexed<rast::shader::inputs::position_normal_uv> cube;
-static rast::mesh::indexed<rast::shader::inputs::position_normal_uv> plane;
 //static thd::thread_pool tp(std::thread::hardware_concurrency());
 static thd::thread_pool tp(std::thread::hardware_concurrency() - 2);
 //static thd::thread_pool tp(1);
 static std::bitset<256> pressed;
 static glm::vec2 mouseDelta = glm::vec2(0.0f);
+
+template <typename Shader>
+struct material { };
+
+template <>
+struct material<rast::shader::lambert_textured> {
+    uint32_t texture_id;
+};
+
+template <typename Shader>
+struct model {
+    rast::mesh::indexed<typename Shader::vertex::input> mesh;
+    material<Shader> mat;
+};
+
+struct object {
+    struct model {
+        uint32_t mesh_id = 0;
+        uint32_t material_id = 0;
+    };
+    std::vector<model> models;
+    glm::mat4 M = glm::mat4(1.0f);
+};
+
+struct scene {
+    using shader = rast::shader::lambert_textured;
+    using mesh_type = rast::mesh::indexed<rast::shader::inputs::position_normal_uv>;
+
+    std::vector<rast::image<rast::color::rgba8>> textures;
+    std::vector<mesh_type> meshes;
+    std::vector<material<shader>> materials;
+    std::vector<object> objects;
+
+    static std::filesystem::path get_full_path(const char* path, const std::filesystem::path& root) {
+        std::filesystem::path res = path;
+        if (res.is_relative()) {
+            res = root;
+            res.append(path);
+        }
+		return res;
+    }
+
+    void load(const char* filename) {
+        std::filesystem::path scene_file_path = filename;
+        scene_file_path.remove_filename();
+		std::ifstream ifs(filename);
+		rapidjson::IStreamWrapper isw(ifs);
+
+		rapidjson::Document document;
+		document.ParseStream(isw);
+		assert(document.IsObject());
+
+        // textures
+        {
+			const rapidjson::Value& textures_node = document["textures"];
+            textures.reserve(textures_node.Size());
+            for (rapidjson::SizeType i = 0; i < textures_node.Size(); ++i) {
+                const rapidjson::Value& texture_node = textures_node[i];
+                std::filesystem::path texture_path = get_full_path(texture_node.GetString(), scene_file_path);
+                textures.emplace_back(rast::image<rast::color::rgba8>::load(
+                    texture_path.string().c_str()
+                ));
+            }
+        }
+        // texture packs
+        std::unordered_map<std::string, uint32_t> texture_pack_map;
+        {
+			const rapidjson::Value& texture_packs_node = document["texture_packs"];
+			for (rapidjson::Value::ConstMemberIterator texture_pack_iter = texture_packs_node.MemberBegin(); texture_pack_iter != texture_packs_node.MemberEnd(); ++texture_pack_iter) {
+				texture_pack_map.insert({ texture_pack_iter->name.GetString(), static_cast<uint32_t>(textures.size())});
+				const rapidjson::Value& texture_pack_node = texture_pack_iter->value;
+
+				std::filesystem::path texture_pack_path = get_full_path(
+					texture_pack_node["path"].GetString(),
+					scene_file_path
+				);
+
+				const rapidjson::Value& textures_node = texture_pack_node["textures"];
+				for (rapidjson::SizeType i = 0; i < textures_node.Size(); i++) {
+					std::filesystem::path tmp = texture_pack_path;
+					tmp.append(textures_node[i].GetString());
+					textures.push_back(rast::image<rast::color::rgba8>::load(
+						tmp.string().c_str()
+					));
+				}
+
+			}
+		}
+
+        // meshes
+        {
+            const rapidjson::Value& meshes_node = document["meshes"];
+			for (rapidjson::SizeType i = 0; i < meshes_node.Size(); ++i) {
+				std::filesystem::path mesh_path = get_full_path(
+					meshes_node[i].GetString(),
+					scene_file_path
+				);
+				if (mesh_path.extension() == ".meshes") {
+					auto tmp_meshes = mesh_type::load_multiple(
+						mesh_path.string().c_str()
+					);
+					for (auto& mesh : tmp_meshes) meshes.push_back(mesh);
+				}
+				else if (mesh_path.extension() == ".mesh") {
+					auto tmp_mesh = mesh_type::load(
+						mesh_path.string().c_str()
+					);
+					meshes.push_back(tmp_mesh);
+				}
+				else throw std::runtime_error("wrong file extension");
+			}
+			for (auto& mesh : meshes) {
+				mesh.process<rast::shader::inputs::position_normal_uv::flip_uv>();
+			}
+        }
+
+
+        // materials
+        {
+			const rapidjson::Value& materials_node = document["materials"];
+			materials.reserve(materials_node.Size());
+			for (rapidjson::SizeType i = 0; i < materials_node.Size(); ++i) {
+				const rapidjson::Value& material_node = materials_node[i];
+				const rapidjson::Value& texture_node = material_node["texture"];
+				uint32_t texture_id = texture_node["index"].GetUint();
+                if (texture_node.HasMember("pack")) {
+                    texture_id += texture_pack_map.at(texture_node["pack"].GetString());
+                }
+				materials.push_back({ texture_id });
+			}
+        }
+
+
+        // objects
+        {
+			const rapidjson::Value& objects_node = document["objects"];
+            objects.reserve(objects_node.Size());
+            for (rapidjson::SizeType i = 0; i < objects_node.Size(); ++i) {
+                const rapidjson::Value& object_node = objects_node[i];
+                object obj;
+
+                if (object_node.HasMember("M")) {
+                    const rapidjson::Value& M_node = object_node["M"];
+                    if (M_node.HasMember("position")) {
+                        obj.M = glm::translate(obj.M, glm::vec3(
+                            M_node["position"][0].GetFloat(),
+                            M_node["position"][1].GetFloat(),
+                            M_node["position"][2].GetFloat()
+                        ));
+                    }
+                    if (M_node.HasMember("scale")) {
+                        obj.M = glm::scale(obj.M, glm::vec3(
+                            M_node["scale"][0].GetFloat(),
+                            M_node["scale"][1].GetFloat(),
+                            M_node["scale"][2].GetFloat()
+                        ));
+                    }
+                }
+
+                const rapidjson::Value& models_node = object_node["models"];
+                obj.models.reserve(models_node.Size());
+                for (rapidjson::SizeType i = 0; i < models_node.Size(); ++i) {
+                    const rapidjson::Value& model_node = models_node[i];
+                    object::model om;
+                    om.mesh_id = model_node["mesh"]["index"].GetUint();
+                    om.material_id = model_node["material"]["index"].GetUint();
+                    obj.models.push_back(om);
+                }
+                objects.push_back(obj);
+            }
+        }
+    }
+
+    template <typename Callable>
+    void draw(const glm::mat4& PV, Callable&& draw_call) const {
+        shader::uniform_buffer ubo;
+        for (const auto& object : objects) {
+            ubo.vertex.PVM = PV * object.M;
+            for (const auto model : object.models) {
+                ubo.fragment.texture = textures[materials[model.material_id].texture_id];
+                draw_call(meshes[model.mesh_id], ubo);
+            }
+        }
+    }
+};
 
 struct application {
     using depth_format = uint32_t;
@@ -65,113 +244,13 @@ struct application {
 
     glm::mat4 V = glm::lookAt(glm::vec3(5.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	glm::mat4 P = glm::perspective(70.0f, 16.0f / 9.0f, near, far);
+    scene scene;
 
     rast::image<depth_format> depth_buffer;
 };
 
 static application app;
 
-template <typename Shader>
-struct model {
-    rast::mesh::indexed<typename Shader::vertex::input> mesh;
-    typename Shader::material material;
-};
-
-struct scene {
-    std::vector<rast::image<rast::color::rgba8>> textures;
-    std::vector<model<rast::shader::lambert_textured>> models;
-
-    void load(const char* filename) {
-        std::filesystem::path scene_file_path = filename;
-        scene_file_path.remove_filename();
-		std::ifstream ifs(filename);
-		rapidjson::IStreamWrapper isw(ifs);
-
-		rapidjson::Document document;
-		document.ParseStream(isw);
-		assert(document.IsObject());
-
-        // textures
-        std::unordered_map<std::string, uint32_t> texture_pack_map;
-        rapidjson::Value& texture_packs = document["resources"]["texture_packs"];
-        for (rapidjson::Value::ConstMemberIterator texture_pack_iter = texture_packs.MemberBegin(); texture_pack_iter != texture_packs.MemberEnd(); ++texture_pack_iter) {
-            texture_pack_map.insert({ texture_pack_iter->name.GetString(), static_cast<uint32_t>(textures.size())});
-
-			std::filesystem::path texture_pack_path = texture_pack_iter->value["path"].GetString();
-			if (texture_pack_path.is_relative()) {
-				texture_pack_path = scene_file_path;
-				texture_pack_path.append(texture_pack_iter->value["path"].GetString());
-			}
-
-			//textures.reserve(texture_pack["textures"].Size());
-			for (rapidjson::SizeType i = 0; i < texture_pack_iter->value["textures"].Size(); i++) {
-				std::filesystem::path tmp = texture_pack_path;
-				tmp.append(texture_pack_iter->value["textures"][i].GetString());
-				textures.push_back(rast::image<rast::color::rgba8>::load(
-					tmp.string().c_str()
-				));
-			}
-
-        }
-
-        // meshes
-        rapidjson::Value& objects = document["objects"];
-        for (rapidjson::SizeType i = 0; i < objects.Size(); ++i) {
-			std::filesystem::path meshes_path = objects[i]["meshes"].GetString();
-			if (meshes_path.is_relative()) {
-				meshes_path = scene_file_path;
-				meshes_path.append(objects[i]["meshes"].GetString());
-			}
-
-			auto meshes = rast::mesh::indexed<rast::shader::inputs::position_normal_uv>::load_multiple(
-				meshes_path.string().c_str()
-			);
-			for (auto& mesh : meshes) {
-				mesh.process<rast::shader::inputs::position_normal_uv::flip_uv>();
-			}
-
-			typename rast::shader::lambert_textured::material shader_mat;
-            shader_mat.M = glm::mat4(1.0f);
-            rapidjson::Value& material = objects[i]["material"];
-            uint32_t albedo_id = texture_pack_map.at(material["albedo"]["pack"].GetString());
-            for (rapidjson::SizeType i = 0; i < material["albedo"]["index"].Size(); ++i) {
-                uint32_t index = material["albedo"]["index"][i].GetUint();
-                shader_mat.texture = rast::texture<rast::color::rgba8>::sampler(textures[albedo_id + index]);
-                shader_mat.M = glm::scale(glm::mat4(1.0f), glm::vec3(0.05f));
-                models.push_back({ meshes[i], shader_mat});
-            }
-        }
-    }
-};
-
-struct sponza {
-    inline static std::vector<rast::mesh::indexed<rast::shader::inputs::position_normal_uv>> meshes = {};
-    inline static std::vector<rast::image<rast::color::rgba8>> textures = {};
-    inline const static std::vector<int> mesh_to_texture = {
-		0, 1, 2, 3, 4, 5, 6, 7, 5, 8,
-		6, 5, 9, 4, 6, 4, 5, 6, 5, 6,
-		5, 6, 5, 6, 5, 6, 5, 6, 5, 6,
-		5, 6, 5, 6, 5, 4, 5, 4, 10, 4,
-		10, 4, 10, 4, 9, 4, 8, 7, 5, 11,
-		12, 4, 13, 0, 14, 15, 16, 14, 15, 14,
-		16, 15, 13, 17, 18, 19, 18, 19, 18, 17,
-		19, 18, 17, 20, 21, 20, 21, 20, 21, 20,
-		21, 1, 2, 1, 2, 1, 2, 1, 2, 1,
-		2, 1, 2, 1, 2, 22, 23, 3, 23, 3, 4, 24, 4
-	};
-
-	static void load() {
-		assert(meshes.capacity() == 0);
-        meshes = rast::mesh::indexed<rast::shader::inputs::position_normal_uv>::load_multiple("private/assets/models/sponza_palace.meshes");
-		for (auto& mesh : meshes) {
-			mesh.process<rast::shader::inputs::position_normal_uv::flip_uv>();
-		}
-		textures.reserve(25);
-		for (int i = 0; i < 25; ++i) {
-			textures.emplace_back(rast::image<rast::color::rgba8>::load(("C:/Models/sponza-sketchfab/gltf/sponza_palace/textures/material_" + std::to_string(i) + "_baseColor.png").c_str()));
-		}
-	}
-};
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -196,18 +275,11 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     app.P = glm::perspective(glm::radians(70.0f), (float)width / height, app.near, app.far);
     app.V = glm::lookAt(glm::vec3(5.0f, 5.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
-    texture = rast::image<rast::color::rgba8>::load("assets/textures/uvChecker1.png");
-    texture2 = rast::image<rast::color::rgba8>::load("assets/textures/neutral_normal.png");
     app.depth_buffer = rast::image<application::depth_format>(width, height);
     //g_buffer = GBuffer(width, height);
     
-    icosphere = rast::mesh::indexed<rast::shader::inputs::position_normal_uv>::load("assets/models/SuzanneSmooth.mesh");
-    icosphere.process(rast::shader::inputs::position_normal_uv::flip_uv);
-    cube = rast::mesh::indexed<rast::shader::inputs::position_normal_uv>::load("assets/models/cube.mesh");
-    cube.process(rast::shader::inputs::position_normal_uv::flip_uv);
-    plane = rast::mesh::indexed<rast::shader::inputs::position_normal_uv>::load("assets/models/plane.mesh");
-    plane.process(rast::shader::inputs::position_normal_uv::flip_uv);
-    sponza::load();
+    app.scene.load("private/assets/scenes/sponza.json");
+    //app.scene.load("assets/scenes/scene.json");
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
@@ -299,48 +371,19 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 
     using shader = rast::shader::lambert_textured;
     using clipper = rast::sutherland_hodgman;
-	rast::scissor scissor(0, 0, framebuf.width(), framebuf.height());
 
+    // record command buffer
     static rast::command_buffer<shader> cmd_buffer;
     cmd_buffer.reset();
 	glm::mat4 PV = app.P * app.V;
-
-    //shader::environment env;
-    //env.PV = PV;
-    //for (auto& model : scene::models) {
-    //    cmd_buffer.draw_indexed(model.mesh, shader::get_ubo(model.material, env), scissor);
-    //}
-
-
-	shader::uniform_buffer ubo;
-    ubo.vertex.PVM = PV * glm::scale(glm::mat4(1.0f), glm::vec3(0.05f));
-	for (int i = 0; i < sponza::meshes.size(); ++i) {
-        ubo.fragment.texture = sponza::textures[sponza::mesh_to_texture[i]];
-        cmd_buffer.draw_indexed(sponza::meshes[i], ubo, scissor);
-    }
-	ubo.fragment.texture = texture;
-	ubo.vertex.PVM = PV * M;
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::translate(M, glm::vec3(3.0f, 0.0f, 0.0f));
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::translate(M, glm::vec3(-3.0f, 0.0f, 0.0f));
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::translate(M, glm::vec3(0.0f, 0.0f, 3.0f));
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::translate(M, glm::vec3(0.0f, 0.0f, -3.0f));
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::translate(M, glm::vec3(3.0f, 0.0f, 3.0f));
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::translate(M, glm::vec3(3.0f, 0.0f, -3.0f));
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::translate(M, glm::vec3(-3.0f, 0.0f, 3.0f));
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::translate(M, glm::vec3(-3.0f, 0.0f, -3.0f));
-	cmd_buffer.draw_indexed(icosphere, ubo, scissor);
-	ubo.vertex.PVM = PV * glm::scale(glm::translate(M, glm::vec3(0.0f, -1.0f, 0.0f)), glm::vec3(3.0f));
-	cmd_buffer.draw_indexed(plane, ubo, scissor);
+    app.scene.draw(PV,
+        [viewport = rast::scissor(0, 0, framebuf.width(), framebuf.height())]
+        (const scene::mesh_type& mesh, const shader::uniform_buffer& ubo)
+        { cmd_buffer.draw_indexed(mesh, ubo, viewport); }
+    );
 	cmd_buffer.submit<clipper>(framebuf, tp);
 
+    // present to screen
 	tp.wait();
     SDL_Rect rect;
     rect.x = 0;
